bf8d8af477f59ca666c1b162fffba69f11e9d7e7
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-03-18 18:39:26 +0530

    demonstrated var function scope and IIFE for block scoping

--- JS/practice1.js
+++ JS/practice1.js
@@ -24,3 +24,26 @@ console.log(a[b]);*/
 //When we are using b and c as the key they are converted to [Object Object] by toString method.
 //So there is only one key in object a which is [Object Object] and the second assignment 456 overrides the first 123.
 //a = {'[object object]' : 456}
+
+/*function show(){
+    {
+        var x = 9;
+        var y = 10;
+    }
+    console.log(x,y);
+}
+show();*/
+//In this case value of x and y will be printed as both have function scope because of var but if we have to
+//limit the scope between {} then simple way is to use let and the log statement will throw a reference err.
+//Another way is to do this using IIFE so even with var scope of x and y will be bounded between {}
+
+/*function show() {
+  {
+    (function () {
+      var x = 9;
+      var y = 10;
+    })();
+  }
+  console.log(x, y);
+}
+show();*/
